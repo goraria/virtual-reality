96b84c74a1a5305260b5186e9221a19968142bc9
goraria <gorariagroup@gmail.com>
2024-10-19 11:33:18 +0700

    Japtor - Config Project

---
+++ Assets/Scripts/NoteController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NoteController : MonoBehaviour {
+    [Header("Input")]
+    [SerializeField] private KeyCode closeKey;
+
+    [Space(10)]
+    [SerializeField] private PersonMovement player;
+
+    [Header("UI Text")]
+    [SerializeField] private GameObject noteCanvas;
+    // [SerializeField] private TMP_Text noteTextAreaUI;
+
+    [Space(10)]
+    [SerializeField]
+    [TextArea] private string noteText;
+
+    [Space(10)]
+    [SerializeField] private UnityEvent openEvent;
+    private bool isOpen = false;
+    public void ShowNote() {
+        // noteTextAreaUI.text = noteText;
+        noteCanvas.SetActive(true);
+        openEvent.Invoke();
+        // DisablePlayer(true);
+        isOpen = true;
+    }
+
+    void DisableNote() {
+        noteCanvas.SetActive(false);
+        // DisablePlayer(false);
+        isOpen = false;
+    }
+
+    private void Update() {
+        if (isOpen) {
+            if (Input.GetKeyDown(closeKey)) {
+                DisableNote();
+            }
+        }
+    }
+}
